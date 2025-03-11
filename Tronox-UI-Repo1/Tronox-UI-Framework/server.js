d1f89217fad54827e5e697d2999cef3655c8dcb2
Gayathri0798 <126227823+Gayathri0798@users.noreply.github.com>
2025-03-11 15:58:42 +0530

    Update server.js

--- Tronox-UI-Repo1/Tronox-UI-Framework/server.js
+++ Tronox-UI-Repo1/Tronox-UI-Framework/server.js
@@ -24,6 +24,11 @@ app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
+
+app.use((req, res, next) => {   
+  res.header("Access-Control-Allow-Origin", "*");   
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");   
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); next(); });
 app.use(bodyParser.json());
 // Fix __dirname in ES Module
 const __filename = fileURLToPath(import.meta.url);
