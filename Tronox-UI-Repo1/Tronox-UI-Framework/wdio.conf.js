31b34a6e4f0d4bc2665dec784b7d40b8a809f39e
Gayathri0798 <gayathri.subbiah@in.ey.com>
2025-03-12 15:14:43 +0530

    Config change

--- Tronox-UI-Repo1/Tronox-UI-Framework/wdio.conf.js
+++ Tronox-UI-Repo1/Tronox-UI-Framework/wdio.conf.js
@@ -65,6 +65,13 @@ export const config = {
         browserName: 'MicrosoftEdge',
         'ms:edgeOptions': {
         binary: '/usr/bin/microsoft-edge', // Use `which microsoft-edge` to confirm the path
+        args: [
+            '--headless', // Run Edge in headless mode
+            '--no-sandbox', // Bypass OS security model
+            '--disable-gpu', // Disable GPU hardware acceleration
+            '--remote-debugging-port=9222', // Specify a debugging port
+            '--disable-dev-shm-usage' // Overcome shared memory issues
+        ]
     }
     },
     {
