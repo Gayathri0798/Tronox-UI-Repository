4f0a669c244e7bd23084c0a903c7d83accce8c56
Gayathri0798 <gayathri.subbiah@in.ey.com>
2025-03-19 15:18:45 +0530

    Config change

--- Tronox-UI-Repo1/Tronox-UI-Framework/wdio.conf.js
+++ Tronox-UI-Repo1/Tronox-UI-Framework/wdio.conf.js
@@ -50,7 +50,10 @@ export const config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'MicrosoftEdge',
+        'ms:edgeOptions': {
+            args: ['--disable-popup-blocking', '--no-sandbox','--headless']
+        }
     }],
  
     //
