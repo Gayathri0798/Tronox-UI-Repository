4cc6160dba24dfa2c3f6aa346095ab281a2faac0
Gayathri0798 <gayathri.subbiah@in.ey.com>
2025-04-09 10:41:47 +0530

    Display log changes

--- Tronox-UI-Repo1/Tronox-UI-Framework/server.js
+++ Tronox-UI-Repo1/Tronox-UI-Framework/server.js
@@ -26,6 +26,7 @@ app.use("/documents", express.static(DOCUMENTS_FOLDER));
 app.use(cors());
 app.use(compression({ flush: zlibConstants.Z_SYNC_FLUSH }));
 app.use(bodyParser.json());
+fs.writeFileSync(logFilePath, "", "utf8"); // Clear on startup
 // Fix __dirname in ES Module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
